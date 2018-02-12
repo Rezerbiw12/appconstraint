2398e20c17184cdc84e4cad17d8860f5d3296d71
teerawut1212 <teerawut1212@hotmail.com>
2018-02-13 01:06:27 +0700

    createnewactivity

--- app/src/main/java/com/pitipong/pitipong1997/myapplication/MainActivity.java
+++ app/src/main/java/com/pitipong/pitipong1997/myapplication/MainActivity.java
@@ -1,12 +1,11 @@
 package com.pitipong.pitipong1997.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,19 +15,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final EditText edtInput = (EditText) findViewById(R.id.edtInput);
-        Button cmdCheck = (Button) findViewById(R.id.cmdCheck);
-        final TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
+        Button cmdCheck1 = (Button) findViewById(R.id.cmdCheck1);
+        Button cmdCheck2 = (Button)findViewById(R.id.cmdCheck2);
 
-        if (cmdCheck != null){
-            cmdCheck.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (edtInput.getText().toString().trim()!= ""){
-                        tvOutput.setText(edtInput.getText());
-                    }
-                    Toast.makeText(MainActivity.this,"อ่าข้อมูลเรียบร้อยแล้ว",Toast.LENGTH_LONG).show();
+
+        cmdCheck1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtInput.getText().toString().trim().equals("")) {
+                    edtInput.setError("กรุณาคีย์ตัวเลขก่อนจ๊ะ!!");
                 }
-            });
-        }
+                 else{
+                        Double tempValue = Double.parseDouble(edtInput.getText().toString().trim());
+                        Double tempResult;
+                        tempResult = ((tempValue*9)/5)+32;
+                        Intent i = new Intent(getApplicationContext(),ResultActivity.class);
+                        i.putExtra("RESULT",String.valueOf(tempResult));
+                        startActivity(i);
+                }
+            }
+        });
+        cmdCheck2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtInput.getText().toString().trim().equals("")) {
+                    edtInput.setError("กรุณาคีย์ตัวเลขก่อนจ๊ะ!!");
+                }
+                else{
+                    Double tempValue = Double.parseDouble(edtInput.getText().toString().trim());
+                    Double tempResult;
+                    tempResult = ((tempValue-32)*5)/9;
+                    Intent i = new Intent(getApplicationContext(),ResultActivity.class);
+                    i.putExtra("RESULT",String.valueOf(tempResult));
+                    startActivity(i);
+                }
+            }
+        });
     }
 }
