588fafe159ee05cee7e727de9831fc29805242e4
teerawut1212 <teerawut1212@hotmail.com>
2018-02-13 13:29:37 +0700

    createnewactivity

---
+++ app/src/main/java/com/pitipong/pitipong1997/myapplication/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.pitipong.pitipong1997.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        Bundle result =getIntent().getExtras();
+        String txtResultValue = result.getString("RESULT");
+
+        TextView txtResult =(TextView)findViewById(R.id.textView3);
+        txtResult.setText(txtResultValue);
+    }
+}
