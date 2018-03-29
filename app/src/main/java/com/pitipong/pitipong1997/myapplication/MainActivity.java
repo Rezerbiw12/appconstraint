3ad0dcde67b12e500b189051193228c88f725a7b
teerawut1212 <teerawut1212@hotmail.com>
2018-03-29 23:41:49 +0700

    29/3/2561

--- app/src/main/java/com/pitipong/pitipong1997/myapplication/MainActivity.java
+++ app/src/main/java/com/pitipong/pitipong1997/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.pitipong.pitipong1997.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +13,19 @@ import android.widget.EditText;
 import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout ;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         final EditText edtInput = (EditText) findViewById(R.id.edtInput);
         /*Button cmdCheck1 = (Button) findViewById(R.id.cmdCheck1);
@@ -72,5 +83,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
